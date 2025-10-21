40aff4af63a34d2864b6e90d9bd76571cb616a13
luli <95196@sistemas.frc.utn.edu.ar>
2025-10-21 11:04:28 -0300

    imp-agrego models

--- practicos/tp6/back/src/modelos/actividad.py
+++ practicos/tp6/back/src/modelos/actividad.py
@@ -1,1 +1,10 @@
+from dataclasses import dataclass
 
+# Clase definida pero sin funciones implementadas
+@dataclass
+class Actividad:
+    id = int
+    nombre = str
+    capacidad_maxima = int
+    requiere_vestimenta = bool
+    edad_minima = int
